245b3742cd5409f1454b9859839c73ee01221208
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-11-18 22:42:29 +0530

    Required Props

---
+++ coin_flipper/src/CoinContainer.js
@@ -0,0 +1,19 @@
+
+
+class CoinContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currCoin: null,
+            nFlips: 0,
+            nHeads: 0,
+            nTails: 0
+        }
+
+
+
+
+    }
+}
+
+export default CoinContainer;
